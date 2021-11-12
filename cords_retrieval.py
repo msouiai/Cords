7c9fee013ed8bf41df05d727ec2c5d65d0e4521a
Mohamed Souiai <msouiai@magicleap.com>
2021-11-11 22:20:16 -0800

    1- Adicinar definicoes vazias 2- mudanca de nome de arquivo 3- execucao e teste

--- cords_retrieval.py
+++ cords_retrieval.py
@@ -1,1 +1,24 @@
-print("Hello World")
+def process_cords_content(cords_content):
+    print("Processing Cord Content")
+
+def read_cords_file(cord_file_name):
+    print("Reading the following Cords File")
+    print(cord_file_name)
+    # Opening file but its not read yet
+    file = open(cords_file_name,'r')
+    # Reading the file
+    # Marcos Magic ***********
+
+
+def read_cords(cord_file_name):
+    print("Reading Cords")
+    # We are reading the file here
+    cords_content = read_cords_file(cord_file_name)
+    # Alright we have the content
+    process_cords_content(cords_content)
+
+
+cords_file_name = "simple_acordes_e_harmonia.txt"
+read_cords(cords_file_name)
+
+
