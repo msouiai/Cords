ee9f89ab56eebf9907ddc137830ae6d9dab86018
Mohamed Souiai <msouiai@magicleap.com>
2021-11-11 22:41:35 -0800

    linha para teste e exercicio

--- cords_retrieval.py
+++ cords_retrieval.py
@@ -17,8 +17,14 @@ def read_cords(cord_file_name):
     # Alright we have the content
     process_cords_content(cords_content)
 
+def look_for_the_line_where_the_cord_is(cord_we_are_looking_for):
+    print("procurar o acord na lista ")
+    print(cord_we_are_looking_for)
 
+    
 cords_file_name = "simple_acordes_e_harmonia.txt"
-read_cords(cords_file_name)
+cord_we_are_looking_for = "C7+"
+look_for_the_line_where_the_cord_is(cord_we_are_looking_for)
+#read_cords(cords_file_name)
 
 
