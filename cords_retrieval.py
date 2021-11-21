652a9d2a6eb40fda3a42397f3bdb4b9b8314bc5a
Marcos Vinicius De Araujo <marcosvmsa@Marcoss-MacBook-Pro-2.local>
2021-11-21 14:21:19 -0800

    Eliminei a leitura em um arquivo e coloquei a leitura direta na tabe Excel, preciso ainda tentar fazer a leitura em todas as abas do excel para ficar mais dinamico. programa funcionando nas condicoes corretas. Ainda e preciso tratar as condicoes de erro. Eliminei uma Def para teste

--- cords_retrieval.py
+++ cords_retrieval.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 def process_cords_content(cords_content):
     print("Processing Cord Content")
 
@@ -5,29 +7,40 @@ def read_cords_file(cord_file_name):
     #print("Reading the following Cords File")
     #print(cord_file_name)
     # Opening file but its not read yet
-    file = open(cords_file_name,'r')
+    """file = open(cords_file_name,'r')
     # Reading the file
     # Marcos Magic ***********
-    for line in file:
-        if cord_we_are_looking_for in line:
-            print(f'Esse e o seu campo harmonico = ', line)
+    if cord_we_are_looking_for == "":
+        print(f'Esse acorde {cord_we_are_looking_for} nao e valido')
+        for line in file:
+            if cord_we_are_looking_for in line:
+                print(f'Esse e o seu campo harmonico = ', line)
     file.close()
+    """
+    x = pd.read_excel('/Users/marcosvmsa/Cords/Acordes_e_Harmonia.xlsx', engine='openpyxl')
+    if cord_we_are_looking_for == x.iloc[29, 2]:
+        print(x.iloc[29, 2:9])
+    else:
+        print(f'Esse acord {cord_we_are_looking_for} nao e valido ')
+    #tb = pd.read_excel('Acordes_e_Harmonia.xlsx')
 
 
 def read_cords(cord_file_name):
     #print("Reading Cords")
-    # We are reading the file here
+    # We are reading the file hereC7+
+
     cords_content = read_cords_file(cord_file_name)
     # Alright we have the content
     process_cords_content(cords_content)
 
-def look_for_the_line_where_the_cord_is(cord_we_are_looking_for):
-    print("procurar o acord na lista ")
-    #print(cord_we_are_looking_for)
+#def look_for_the_line_where_the_cord_is(cord_we_are_looking_for):
+        # print(cord_we_are_looking_for)
+        #print("procurar o acord na lista ")
+
 
 cords_file_name = "simple_acordes_e_harmonia.txt"
 cord_we_are_looking_for = str(input('Digite o Acorde = '))
-look_for_the_line_where_the_cord_is(cord_we_are_looking_for)
+#look_for_the_line_where_the_cord_is(cord_we_are_looking_for)
 read_cords(cords_file_name)
 
 
